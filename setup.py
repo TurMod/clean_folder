f8ca38489261b062b691ec132f1b0acaa9038f27
TurMod <turbo.moderator@gmail.com>
2023-04-15 21:02:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Mykhailo Gordiichuk',
     author_email='turbo.moderator@gmail.com',
     license='Apache Software',
-    packages=find_namespace_packages(),
+    packages=find_namespace_packages(include=['clean_folder', 'clean_folder.*']),
     entry_points={'console_scripts': ['clean-folder = clean_folder.clean:launch_script']}
 )
