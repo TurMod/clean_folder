67290fccbc98dde930c5aad0c1c46e10b087ddb0
TurMod <turbo.moderator@gmail.com>
2023-04-15 20:22:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     name='clean_folder',
     version='1',
     description='This module will sort your folder.'
-    url=''
+    url='https://github.com/TurMod/clean_folder'
     author='Mykhailo Gordiichuk',
     author_email='turbo.moderator@gmail.com',
+    license='Apache Software',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:launch_script']}
 )
