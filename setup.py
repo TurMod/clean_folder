43adfb2226f649ad911c7f38890b3738f6184136
TurMod <turbo.moderator@gmail.com>
2023-04-15 20:08:11 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name='clean_folder',
+    version='1',
+    description='This module will sort your folder.'
+    url=''
+    author='Mykhailo Gordiichuk',
+    author_email='turbo.moderator@gmail.com',
+)
